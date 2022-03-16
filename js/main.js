c2ba47565840dcafc08a766fa543cc2d78fa54ec
unknown <brova.kat@gmail.com>
2022-03-16 09:56:32 +0300

    добавила но не все

--- js/main.js
+++ js/main.js
@@ -2,20 +2,19 @@ const returnNumber = function getRandomIntInclusive(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   if (min>=max){
-    console.log(-1); }
+    return false
+  };
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
 // источник https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
-
 console.log(returnNumber (0, 10));
 
 const leightString = function сheckStringLenght ( inputText, maxlenght){
-if (inputText.length > maxlenght){
-  console.log('Слишком длинный комментарий');
+if(inputText.length > maxlenght){
   return false;
-}
+};
 return true;
 };
 console.log(leightString ('комментарииииииииииииииии',40));
